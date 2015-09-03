1e7e7d6cd1470c926b50a4c76b655757acd30e8b
Chris Stavitsky <corridorr@gmail.com>
2015-09-03 17:32:17 -0500

    add view functionality

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,4 +1,5 @@
 get '/users' do
   @user = User.find_by(params[:user])
+  p params
   erb :'show'
 end
--- db/migrate/20150903140208_create_users.rb
+++ db/migrate/20150903140208_create_users.rb
@@ -1,10 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string   :nickname
-      t.string   :password
 
-      t.timestamps null: false
-    end
   end
 end
